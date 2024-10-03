f53c62a38e44c229b76ebe7a9b958342699a8294
Farouk Ehab Fouad <96669463+Farouk157@users.noreply.github.com>
2024-10-03 22:13:51 +0300

    Delete FoodPlanner_App directory

--- FoodPlanner_App/app/src/main/java/com/example/foodplanner/mealdetails/view/MealDetailsView.java
+++
@@ -1,8 +0,0 @@
-package com.example.foodplanner.mealdetails.view;
-
-import com.example.foodplanner.mealofday.model.Meal;
-
-public interface MealDetailsView {
-    void showMealDetails(Meal meal);
-    void showError(String message);
-}
--- FoodPlanner_App/app/src/main/java/com/example/foodplanner/mealofday/model/MealRepositoryImp.java
+++
@@ -1,5 +0,0 @@
-package com.example.foodplanner.mealofday.model;
-
-public class MealRepositoryImp {
-
-}
